247d56308557a12dd8aaa236689d11671640cbde
Erick Vilcica <erick.vilcica@protonmail.com>
2021-08-31 17:57:39 +0100

    Add mpris metadata getter using playerctl to waybar

---
+++ waybar/custom_modules/custom-getplayerctl.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+playing_title=$(playerctl metadata --format "{{ title }}")
+playing_artist=$(playerctl metadata --format "{{ artist }}")
+
+status=$(playerctl metadata --format "{{ uc(status) }}")
+
+if [ $status == "PLAYING" ]
+then
+	formatted_status=""
+elif [ $status == "PAUSED" ]
+then
+	formatted_status=""
+fi
+
+#The following commands don't work as expected on my system for some reason. weird.
+#song_length=$(playerctl metadata --format "{{ duration(mpris:length) }}")
+#current_position=$(playerctl metadata --format "{{ duration(position) }}")
+#time_remaining=$(playerctl metadata --format "{{ duration(mpris:length - position) }}")
+
+echo '{"text": "'$formatted_status' '$playing_title' - '$playing_artist'", "class": "custom-getplayerctl", "tooltip": "<b>Title: </b>'$playing_title'\n<b>Artist: </b>'$playing_artist'"}'
+
+# No tooltip ver
+#echo '{"text": "'$formatted_status' '$playing_title' - '$playing_artist'  ", "class": "custom-getplayerctl"}'
