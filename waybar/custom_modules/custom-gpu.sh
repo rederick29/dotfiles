c8de7b6310154e026b9bcf7160d286b8ca6bb80c
Erick Vilcica <erick.vilcica@protonmail.com>
2022-07-18 20:37:02 +0100

    change gpu no again

--- waybar/custom_modules/custom-gpu.sh
+++ waybar/custom_modules/custom-gpu.sh
@@ -4,10 +4,10 @@ raw_clock=$(cat /sys/class/drm/card0/device/pp_dpm_sclk | egrep -o '[0-9]{0,4}Mh
 #clock=$(echo "scale=1;$raw_clock/1000" | bc | sed -e 's/^-\./-0./' -e 's/^\./0./')
 clock=$(echo $raw_clock)
 
-raw_temp=$(cat /sys/class/drm/card0/device/hwmon/hwmon0/temp1_input)
+raw_temp=$(cat /sys/class/drm/card0/device/hwmon/hwmon1/temp1_input)
 temperature=$(($raw_temp/1000))
 
-busypercent=$(cat /sys/class/hwmon/hwmon0/device/gpu_busy_percent)
+busypercent=$(cat /sys/class/hwmon/hwmon1/device/gpu_busy_percent)
 
 deviceinfo=$(glxinfo -B | grep 'Device:' | sed 's/^.*: //')
 driverinfo=$(glxinfo -B | grep "OpenGL version")
